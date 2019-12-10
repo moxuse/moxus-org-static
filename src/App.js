c754c6d3cb3c162cfbd6d4e19d54a2c69577e692
moxuse <moxuse@gmail.com>
2019-12-10 16:29:14 +0900

    added work page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
+import React from "react";
+import { Root, Routes, addPrefetchExcludes } from "react-static";
 //
-import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
+import { Link, Router } from "components/Router";
+import Work from "containers/Work";
 
-import './app.css'
+import "./app.css";
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
+addPrefetchExcludes(["work"]);
 
 function App() {
   return (
     <Root>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
+        <Link to="/">top</Link>
+        <Link to="/about">cv</Link>
+        <Link to="/work">work</Link>
+        <Link to="/blog">blog</Link>
       </nav>
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Dynamic path="dynamic" />
+            <Work path="work" />
             <Routes path="*" />
           </Router>
         </React.Suspense>
       </div>
     </Root>
-  )
+  );
 }
 
-export default App
+export default App;
--- static.config.js
+++ static.config.js
@@ -1,36 +1,55 @@
-import path from 'path'
-import axios from 'axios'
+import path from "path";
+import axios from "axios";
+
+import { makePageRoutes } from "react-static/node";
 
 export default {
   getRoutes: async () => {
     const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
+      "https://jsonplaceholder.typicode.com/posts"
+    );
 
     return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          template: 'src/containers/Post',
+      ...makePageRoutes({
+        items: posts,
+        pageSize: 5,
+        pageToken: "page", // use page for the prefix, eg. blog/page/3
+        route: {
+          // Use this route as the base route
+          path: "blog",
+          template: "src/pages/blog"
+        },
+        decorate: (posts, i, totalPages) => ({
+          // For each page, supply the posts, page and totalPages
           getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
+            posts,
+            currentPage: i,
+            totalPages
+          })
+        })
+      }),
+      // {
+      //   path: "/blog",
+      //   getData: () => ({
+      //     posts
+      //   }),
+      ...posts.map(post => ({
+        path: `/post/${post.id}`,
+        template: "src/containers/Post",
+        getData: () => ({
+          post
+        })
+      }))
+    ];
   },
   plugins: [
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      require.resolve("react-static-plugin-source-filesystem"),
       {
-        location: path.resolve('./src/pages'),
-      },
+        location: path.resolve("./src/pages")
+      }
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
-  ],
-}
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap")
+  ]
+};
