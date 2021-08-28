f0e5e52f302dbbdef915ae2d6206830ec37b2a91
moxuse <moxuse@gmail.com>
2021-08-28 15:20:46 +0900

    added test code

--- components/Header/header.test.tsx
+++ components/Header/header.test.tsx
@@ -1,12 +1,12 @@
 import '@testing-library/jest-dom'
 import * as React from 'react'
 import {render, fireEvent, screen} from '@testing-library/react'
-import Menu from '.';
+import Header  from '.';
 
 describe('Menu', () => {
   it('render correctly', async () => {
-    render(<Menu />);
+    render(<Header />);
 
-    expect(await screen.getByText('work')).toBeVisible();
+    expect(await screen.getByText('moxus.org')).toBeInTheDocument();
   });
 })
