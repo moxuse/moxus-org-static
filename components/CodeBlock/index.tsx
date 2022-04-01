b86d6d0bdd65b3c4d6739a3a1d7b74e39f619b07
moxuse <moxuse@gmail.com>
2022-04-02 00:00:29 +0900

    use registerLanguage

--- components/CodeBlock/index.tsx
+++ components/CodeBlock/index.tsx
@@ -3,8 +3,11 @@
  * CodeBlock.jsx
  */
 
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+ import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+ import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
+ import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco';
+ 
+ SyntaxHighlighter.registerLanguage('javascript', js);
 
 interface CodeBlockProps {
   // children?: string
@@ -17,7 +20,7 @@ const CodeBlock: React.FC<CodeBlockProps> = (props): JSX.Element => {
     <SyntaxHighlighter
       language={props.language || "javascript"} 
       useInlineStyles={props.inline} 
-      style={tomorrow}
+      style={docco}
     >
       {props.children}
     </SyntaxHighlighter>
